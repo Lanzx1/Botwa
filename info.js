3c7c0c5022b53adb9aa3696bb65845299f2144fa
Lanzx1 <137268639+Lanzx1@users.noreply.github.com>
2023-06-23 10:21:10 +0700

    lib

---
+++ info.js
@@ -0,0 +1,31 @@
+exports.info = (id, LanzxBot, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
+	return `🐼 ${LanzxBot} 🐼
+  
+HALO *${id.split("@s.whatsapp.net")[0]}*
+"
+APA KABAR? SEMOGA BAIK-BAIK SAJA!
+
+INFO BOT! :
+🔱 *AUTHOR*: REUZ
+🔱 *GITHUB*: https://github.com/benisadewa/kntnbotwa
+🔱 *INSTAGRAM*: ${https://instagram.com/lanzxz1?igshid=OTk0YzhjMDVlZA==}
+🔱 *YOUTUBE*: ${youtube}
+🔱 *TELEGRAM* : ${belom ada}
+🔱 *WHATSAPP* : 085894445394
+
+INFO LAIN! :
+
+BOT INI MENGGUNAKAN SOURCE CODE YANG SAYA DAPATKAN DAN SAYA MODIFIKASI DENGAN PENAMBAHAN, PENGURANGAN, DAN PERUBAHAN BEBERAPA FITUR.
+BOT INI DIBUAT MENGGUNAKAN NODE.JS
+
+THANKS TO: 
+-ALLAH SWT
+-MUHAMMAD SAW
+-AYAH & IBU
+-ABDUL MUTTAQIN(FDCIABDUL)
+KARNA TELAH MEMBANTU 
+
+bot aktif selama : ${kapanbotaktif}
+JANGAN LUPA FOLLOW instagram
+ ${Lanzx/https://instagram.com/lanzxz1?igshid=OTk0YzhjMDVlZA==}
+}
---
+++ menu.js
@@ -0,0 +1,131 @@
+exports.menu = (id, LanzxBot, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, Lanzx, 100tahunsekali, grupch1, grupch2) => {
+	return `🐼*${LanzxBot}*🐼
+  
+HALO *${id.split("@s.whatsapp.net")[0]}* 
+APA KABAR? SEMOGA BAIK-BAIK SAJA!
+
+📅*${tampilTanggal}* 🔱
+⏰*${tampilWaktu}* 🔱
+(Waktu Server)
+
+🗒KUMPULAN COMMAND ${LanzxBot}:
+ 
+🐼*FUN*🐼
+
+🐼 _*!pantun*_
+_${LanzxBot}_🍄akan mengirimkanmu pantun secara random
+
+🐼 _*!animepict*_
+_${LanzxBot}_🍄akan mengirimkanmu gambar anime secara random
+
+🐼 _*!sticker*_
+_${LanzxBot}_🍄akan membuatkan sticker dari gambar yang kamu kirimkan
+
+Pengggunaan : Kirimkan gambarmu dengan caption !sticker
+
+🐼 _*!nulis <teks>*_
+_${LanzxBot}_🍄akan menuliskan teks yang kamu kirimkan
+
+Contoh: !nulis pasti nana bangka dadang ko bang jamping jamping 
+
+🐼 _*!quotes*_
+_${LanzxBot}_🍄akan mencarikanmu quotes secara random
+
+🐼 _*!pict <cewek/cowok>*_
+_${LanzxBot}_🍄akan mengirimkanmu gambar cewek/cowok secara random
+Contoh: !pict cowok
+
+🐼 _*!animepict*_
+_${LanzxBot}_🍄akan mengirimkanmu gambar anime secara random
+
+🐼 _*!say <teks>*_
+_${LanzxBot}_🍄akan mengirimkan kembali teks yang kamu kirimkan
+Contoh: !say buset BRO LU GANTENG BANGET AWOKWOKWOKWOWKWOK
+
+🐼 _*!lirik*_ <penyanyi-judul lagu>
+_${LanzxBot}_🍄akan mengirimkanmu lirik lagu yang kamu inginkan
+contoh : !lirik Lisa-Gurenge
+
+🐼 _*!alay*_ <teks>
+_${LanzxBot}_🍄akan mengubah teks yang kamu kirimkan menjadi alay
+contoh : !alay ampun bang jago
+
+
+☪*ISLAM*☪
+
+🐼 _*!sholat <daerah>*_
+_${LanzxBot}_🍄akan mengirimkan jadwal sholat sesuai dengan daerah yang kamu kirimkan
+
+Penggunaan 	: !sholat + daerah kamu
+Contoh		: !sholat Surabaya
+
+🐼 _*!quran*_
+_${LanzxBot}_🍄akan mengirimkanmu ayat Al-Quran secara random
+
+📥*DOWNLOADER*📥
+
+🐼 _*!yt <link>*_
+_${LanzxBot}_🍄akan mendownloadkan video youtube sesuai dengan link yang kamu kirimkan.
+Contoh: !yt https://youtu.be/linkamu
+
+🐼 _*!ytmp3 <link>*_
+_${LanzxBot}_🍄akan mengubah video youtube menjadi audio sesuai dengan link yang kamu kirimkan
+
+Contoh: !ytmp3 https://youtu.be/linkamu
+
+🐼 _*!ig <link>*_
+_${LanzxBot}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+
+Contoh: !ig https://instagram.com/linkamu
+
+🐼 _*!fb <link>*_ //ERROR//
+_${LanzxBot}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+
+Contoh: !fb https://facebook.com/linkamu
+
+🐼 _*!twt <link>*_
+_${LanzxBot}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+
+Contoh: !twt https://twitter.com/linkamu
+
+🎭 _*!tiktok <link>*_
+
+_${LanzxBot}_ akan mengirimkanmu video dari link tiktok yang kamu kirimkan
+
+🖥*EDUKASI*🖥
+
+🐼 _*!wikia <yang kamu cari>*_
+_${LanzxBot}_🍄akan mencarikan yang kamu mau di wikipedia
+
+Contoh: !wikia Adolf Hitler
+
+🐼 _*!covid*_
+_${LanzxBot}_🍄akan menampilkan data tentang COVID-19 di Indonesia
+
+🐰*PRIMBON*🐰
+
+🐼 _*!nama <namakamu>*_
+_${LanzxBot}_🍄akan mencarikan apa arti dari nama kamu
+
+Contoh : !nama Stalin
+
+🐼 _*!pasangan <namamu & pasangan>*_
+_${LanzxBot}_🍄akan mencarikan kecocokan antara kamu dan pasanganmu
+
+Contoh : !pasangan covad & covid
+
+
+⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
+FOLLOW INSTAGRAM SAYA
+${instagram}
+
+• Youtube : Lanzx
+• WA : 081271699856
+• Pasang Iklan? Chat Whatsapp.
+
+📺 *IKLAN* : *YOUTUBE Lanzx*
+
+🏚SELALU GUNAKAN MASKER SAAT KELUAR!🏚
+🏚SEMOGA SEHAT SELALU!🏚
+`
+}
